75eedbf4ae5ca83e4f4ea9cdc888e5b4cbe37346
Jose Fernando Guijarro Pomboza <joseguijarro@cne.gob.ec>
2019-05-17 16:18:35 -0500

    Initial commit

---
+++ app/src/main/java/ec/aj/com/mp_envio/logIn/LogIn.java
@@ -0,0 +1,67 @@
+package ec.aj.com.mp_envio.logIn;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import ec.aj.com.mp_envio.ui.login.LoginActivity;
+
+import ec.aj.com.mp_envio.R;
+
+public class LogIn extends AppCompatActivity {
+
+    protected EditText usu;
+    EditText con;
+    Button ingreso;
+    SharedPreferences prefs ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+        cargar();
+        validarRegistro();
+    }
+
+    private void validarRegistro() {
+       /* Boolean yourLocked = prefs.getBoolean("register", false);
+        if(yourLocked){
+            abrirMenu();
+        }*/
+    }
+
+    public void cargar(){
+
+        usu = (EditText) findViewById(R.id.input_email);
+        con = (EditText) findViewById(R.id.input_password);
+        ingreso = (Button) findViewById(R.id.btn_login);
+
+        ingreso.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                validar();
+            }
+        });
+    }
+    public void validar(){
+
+        if(usu.getText().toString().trim().equals("usuario")&& con.getText().toString().trim().equals("123") ) {
+            prefs.edit().putBoolean("register", true).commit();
+            abrirMenu();
+        }
+        else {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("Usuario/Contraseña Incorrecta");
+            alertDialogBuilder.show();
+        }
+    }
+    public void abrirMenu(){
+        Intent intent;
+        intent = new Intent(this, LoginActivity .class);
+        startActivity(intent);
+        finish();
+    }
+}
